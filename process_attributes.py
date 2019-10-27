a1b646306bb422677cda472b6a0e77625c755ace
Adam Winchell <winchell2326adam@gmail.com>
2019-10-27 10:35:00 -0600

    finished processing unemployment

--- process_attributes.py
+++ process_attributes.py
@@ -2,35 +2,54 @@ import pandas as pd
 import pickle
 import numpy as np
 
+
+def pickle_out(filename, object_to_dump):
+    with open(filename, 'wb') as pickleFile:
+        pickle.dump(object_to_dump, pickleFile)
+        pickleFile.close()
+
 def pickle_in(filename):
     with open(filename, 'rb') as pickleFile:
         return pickle.load(pickleFile)
 
 if __name__ == '__main__':
-    county_to_idx = pickle_in('county_to_idx')
+    county_to_idx = pickle_in('processed_data/county_to_idx')
 
     n = len(county_to_idx)
 
     levels_of_education = 4
-    education_attributes = {1970:np.zeros((n, levels_of_education)), 1980:np.zeros((n, levels_of_education)),1990:np.zeros((n, levels_of_education)),2000:np.zeros((n, levels_of_education)), 2013:np.zeros((n, levels_of_education))}
-
-    df = pd.read_excel('Education.xls')
+    years = [1970,1980,1990,2000,2013]
+    attributes = {year: np.full((n,levels_of_education),np.nan) for year in years}
+    df = pd.read_excel('raw_data/Education.xls')
 
     for row in df.iloc[4:].itertuples(index=False):
         county = row[2]+', '+row[1]
         if county in county_to_idx:
             idx = county_to_idx[county]
             for i in range(4):
-                education_attributes[1970][idx][i] = row[11+i]
-                education_attributes[1980][idx][i] = row[19 + i]
-                education_attributes[1990][idx][i] = row[27 + i]
-                education_attributes[2000][idx][i] = row[35 + i]
-                education_attributes[2013][idx][i] = row[43 + i]
+                attributes[1970][idx][i] = row[11+i]
+                attributes[1980][idx][i] = row[19 + i]
+                attributes[1990][idx][i] = row[27 + i]
+                attributes[2000][idx][i] = row[35 + i]
+                attributes[2013][idx][i] = row[43 + i]
 
-    for year,mat in education_attributes.items():
-        np.save('education_'+str(year), mat)
+    for year,mat in attributes.items():
+        pickle_out('education_'+str(year), mat)
 
 
-    df = pd.read_excel('Unemployment.xls')
-    df = pd.read_excel('PovertyEstimates.xls')
-    df = pd.read_excel('PopulationEstimates.xls')
+    df = pd.read_excel('raw_data/Unemployment.xls')
+    years = list(range(2007,2019))
+    attributes = {year:np.full((n,1),np.nan) for year in years}
+
+    for row in df.loc[6:].itertuples(index=False):
+        county = row[2]
+        if county in county_to_idx:
+            idx = county_to_idx[county]
+            for i, year in enumerate(years):
+                attributes[year][idx] = row[9+4*i]
+
+    for year, mat in attributes.items():
+        pickle_out('unemployment_'+str(year), mat)
+
+    # df = pd.read_excel('raw_data/PovertyEstimates.xls')
+    # df = pd.read_excel('raw_data/PopulationEstimates.xls')
