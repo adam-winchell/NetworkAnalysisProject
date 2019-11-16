8cf70b9ae0b35b62bf5a5813d370101343f5ee9f
Adam Winchell <winchell2326adam@gmail.com>
2019-11-16 09:43:30 -0700

    fixed bug in processing

--- process_adjaceny.py
+++ process_adjaceny.py
@@ -11,37 +11,34 @@ def pickle_out(filename, object_to_dump):
 if __name__ == '__main__':
     file = open('raw_data/county_adjacency_raw.txt', 'r', errors='ignore')
 
-    r = re.compile('(\w+\sCounty, \w+)')
-    r1 = re.compile('(\w+\sMunicipo, \w+)')
-    r2 = re.compile('(\w+\sDistrict, \w+)')
-    r3 = re.compile('(\w+\sIsland, \w+)')
-    r4 = re.compile('(\w+\sMunicipality, \w+)')
-    r5 = re.compile('(\w+\sGU, \w+)')
-    #note that Manu'a District, AS gets captured wrong but thats okay
+    # patt = re.compile('\\"(.+)\\"\s+\d+\s+\\"(.+)\\"\s+\d+')
+    patt = re.compile('\\"(.+)\\"')
 
     current_county = ''
     adj_list = defaultdict(list)
     to_idx = {}
     curr_idx = 0
     for line in file:
-        for patt in [r,r1,r2,r3,r4,r5]:
-            s= re.findall(patt,line)
-            if s != []:
-                if len(s) == 2:
-                    current_county = s[0]
+        s= re.findall(patt,line)
+        if s != []:
+            s = s[0].split('"')
+            print(s)
 
-                    adj_list[current_county].append(s[1])
+            if len(s) == 3:
+                current_county = s[0]
 
-                    if s[1] not in to_idx:
-                        to_idx[s[1]] = curr_idx
-                        curr_idx += 1
-                else:
-                    adj_list[current_county].append(s[0])
+                adj_list[current_county].append(s[2])
 
-                if s[0] not in to_idx:
-                    to_idx[s[0]] = curr_idx
+                if s[2] not in to_idx:
+                    to_idx[s[2]] = curr_idx
                     curr_idx += 1
-                break
+            else:
+                adj_list[current_county].append(s[0])
+
+            if s[0] not in to_idx:
+                to_idx[s[0]] = curr_idx
+                curr_idx += 1
+
 
     pickle_out('processed_data/county_to_idx', to_idx)
 
